9dda398c56ee4541d908b6a02c161a974559019f
RAZIN M <razinm@RAZINs-MacBook-Air.local>
2025-09-22 14:31:01 +0530

    made data struct for flight

---
+++ BackEnd/DataModel.py
@@ -0,0 +1,10 @@
+class Flight:
+    def __init__(self, flight_number,dst,duration,fuel_load,type):
+        self.flight_number = flight_number
+        self.dst = dst
+        self.duration = duration
+        self.fuel_load = fuel_load
+        self.type = type
+
+
+
